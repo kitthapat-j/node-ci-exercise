8dd6f1ed017e23dc3bdec9e741f24132f0c666aa
atit <atit@gmail.com>
2025-08-31 02:30:38 +0700

    Add Eslint 2

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+// eslint.config.js
+import eslint from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  eslint.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+];
